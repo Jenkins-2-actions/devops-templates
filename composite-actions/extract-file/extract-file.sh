ad531e40fd3de68cbec25ac69edfe462983c1927
Wilson Hurtado <wilson.hurtado@segurosbolivar.com>
2024-01-03 21:50:15 -0500

    Corrección en la forma de pasar el argumento al script de bash

--- composite-actions/extract-file/extract-file.sh
+++ composite-actions/extract-file/extract-file.sh
@@ -1,8 +1,8 @@
-properties=$(jq -c '.' ${{ inputs.file-path }})
+properties=$(jq -c '.' "$1")
 if [[ $properties ]]; then
   echo "Extracted properties: $properties"
   echo "properties=$properties" >> $GITHUB_OUTPUT
 else
-  echo "ERROR:  ${{ inputs.file-path }} is not defined in repository"
+  echo "ERROR: $1 is not defined in repository"
   exit 1
 fi
