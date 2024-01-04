87422387c4dc8494719999d5852eda96b996e0a7
Wilson Hurtado <wilson.hurtado@segurosbolivar.com>
2024-01-04 01:45:26 -0500

    Prueba de iteración sobre el archivo properties.json

--- composite-actions/extract-file/extract-file.sh
+++ composite-actions/extract-file/extract-file.sh
@@ -2,18 +2,32 @@
 properties=$(jq -c '.' "$1")
 if [ -n "$properties" ]; then
   echo "Extracted properties: $properties"
-  # echo "properties=$properties" >> $GITHUB_OUTPUT
-  # Iterar sobre las claves y valores del JSON
-  while IFS= read -r line; do
-    key=$(echo "$line" | jq -r keys_unsorted[])
-    value=$(echo "$line" | jq -r ".$key")
+  keys=$(echo "$properties" | jq 'keys[]' | tr -d '\r')
+  for key in $keys; do
+    echo "Key to extract: .$key"
+    value=$(echo "$properties" | jq ".$key")
     
     # Crear una variable para cada propiedad
     # ADVERTENCIA: Posible inyección de código malicioso
-    eval "$key=\"$value\""
+    echo "$key:$value"
+    echo "$key=$value" >> $GITHUB_OUTPUT
+  done
+
+  # echo "properties=$properties" >> $GITHUB_OUTPUT
+  # Iterar sobre las claves y valores del JSON
+  # while IFS="," read -r key; do
+  #   echo "START"
+  #   echo "Key -- .$key finish"
+  #   echo "Properties -- $properties"
+  #   value=$(echo "$properties" | jq -r ".$key")
+  #   # key=$(echo "$line" | jq -r keys_unsorted[])
     
-    echo "$key: $value"
-  done <<< "$properties"
+  #   # Crear una variable para cada propiedad
+  #   # ADVERTENCIA: Posible inyección de código malicioso
+  #   echo "Key: $key = Value: $value"
+  #   # eval "$key=\"$value\""
+    
+  # done <<< "$(echo "$properties" | jq -r 'keys[]')"
 else
   echo "ERROR: $1 is not defined in repository"
   exit 1
