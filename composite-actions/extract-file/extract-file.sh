809f87db3db1f92e13ac9cfdb72baca95492d6d3
Wilson Hurtado <wilson.hurtado@segurosbolivar.com>
2024-01-03 23:34:35 -0500

    Prueba de extracción dinámica de parámetros

--- composite-actions/extract-file/extract-file.sh
+++ composite-actions/extract-file/extract-file.sh
@@ -2,7 +2,18 @@
 properties=$(jq -c '.' "$1")
 if [ -n "$properties" ]; then
   echo "Extracted properties: $properties"
-  echo "properties=$properties" >> $GITHUB_OUTPUT
+  # echo "properties=$properties" >> $GITHUB_OUTPUT
+  # Iterar sobre las claves y valores del JSON
+  while IFS= read -r line; do
+    key=$(echo "$line" | jq -r keys_unsorted[])
+    value=$(echo "$line" | jq -r ".$key")
+    
+    # Crear una variable para cada propiedad
+    # ADVERTENCIA: Posible inyección de código malicioso
+    eval "$key=\"$value\""
+    
+    echo "$key: $value"
+  done <<< "$properties"
 else
   echo "ERROR: $1 is not defined in repository"
   exit 1
